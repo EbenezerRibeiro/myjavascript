e3c652bdc380d31b76a918120a2774517ffca564
Ebenezer Ribeiro (Bene) <81185330+EbenezerRibeiro@users.noreply.github.com>
2021-07-05 19:35:59 -0300

    Add files via upload

---
+++ man.js
@@ -0,0 +1,40 @@
+//AULA 1
+//var nome = "Ebenezer Ribeiro";
+//var idade = 19;
+//var frase = "Bene é a melhor pessoa do mundo"
+//alert( nome + " tem " + idade + " anos ");
+//console.log(idade);
+//console.log(nome);
+//console.log(frase);
+
+
+//AULA2 (Array e dicionario)
+//var lista = ["maça", "pera", "banana"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" "));
+
+//AULA 3 (condicionais, laços de repetição e date)
+//var idade = prompt("Qual a sua idade?");
+//if (idade >= 18){
+    //alert("maior de idade");
+//}else{
+    //alert("menor de idade");
+//};
+//var d = nwe Date();
+//alert(d.getDay());
+//alert(d.getHours());
+//alert(d.getMinut());
+
+//Aula 4 (desenvolva pagina web)
+function soma(n1, n2){
+    return n1 + n2;
+}
+function setReplace(frase, nome, novo_nome){
+     return frase.replace(nome, novo_nome)
+
+}
+alert(soma(5, 10));
+alert(setReplace("vai pão", "não pao"));
