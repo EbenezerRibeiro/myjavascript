1845fb894f3bc45898a20f2fb7c548dfd1b9df7d
EbenezerRibeiro <ebenezerribeiro020@hotmail.com>
2021-08-22 10:16:05 -0300

    Arualizando aulas

--- aulas2.js
+++ aulas2.js
@@ -17,5 +17,41 @@ console.log(sum(5, 5, 5, 2, 3));
 
 // strings, arrays, literal objects e objetos ileraveis
 const str = 'Digital innovation one';
+//const arr = [1, 2, 3, 4];
+function logArgs(...args) {
+    console.log(args); // console.log(arguments);
+}
 
-function logArgs
+logArgs(...str);
+
+//outro exemplo
+const srt = 'Digital innovation one';
+const arr = [1, 2, 3, 4];
+
+function logArgs(a, b, c) {
+    console.log(a, b, c);
+}
+const arrClone = [...arr];
+const obj = {
+    test:123
+};
+const obj2 = {
+    ...obj,t
+    test2: 'Hello'
+};
+console.log(obj2);
+
+//PARTE 2 DA AULA (como usar destrucring em ReactJS)
+
+var arr = ['Apple', 'Banana', 'Orange'];
+
+var apple = arr[0];
+var banana = arr[1];
+var orange = arr[2];
+var tomato = arr[3][0];
+
+//Destructuring Assignment
+
+var [apple2, banana2, orange2, [tomat2]] = ['Apple', 'Banana', 'Orange', [tomato2]];
+
+console.log(apple, apple2);
